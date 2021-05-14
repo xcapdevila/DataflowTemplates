a61074d549ba03d32aa43a155f122b7683d838d2
Xavier Capdevila <xavier.capdevila@fonyou.com>
2021-05-14 14:03:59 +0200

    Fix use NestedValueProvider to manipulate option arg

--- src/main/java/com/google/cloud/teleport/templates/custom/DatastoreToDatastoreDeleteCurrentDate.java
+++ src/main/java/com/google/cloud/teleport/templates/custom/DatastoreToDatastoreDeleteCurrentDate.java
@@ -18,6 +18,7 @@ package com.google.cloud.teleport.templates.custom;
 
 import com.google.cloud.teleport.templates.common.DatastoreConverters.DatastoreDeleteEntityJson;
 import com.google.cloud.teleport.templates.common.DatastoreConverters.DatastoreDeleteOptions;
+import com.google.cloud.teleport.templates.common.DatastoreConverters.DatastoreReadOptions;
 import com.google.cloud.teleport.templates.common.DatastoreConverters.ReadJsonEntities;
 import com.google.cloud.teleport.templates.common.JavascriptTextTransformer.JavascriptTextTransformerOptions;
 import com.google.cloud.teleport.templates.common.JavascriptTextTransformer.TransformTextViaJavascript;
@@ -25,11 +26,9 @@ import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.options.Default;
-import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.options.ValueProvider.NestedValueProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,53 +40,6 @@ public class DatastoreToDatastoreDeleteCurrentDate {
   private static final Logger LOG = LoggerFactory.getLogger(DatastoreToDatastoreDeleteCurrentDate.class);
   private static final String NOW_PATTERN = "\\$\\{NOW(\\+|\\-)(\\d*?)(y|m|d|h)\\}";
   private static final Pattern PATTERN = Pattern.compile(NOW_PATTERN);
-
-  /**
-   * Custom PipelineOptions.
-   */
-  public interface DatastoreToDatastoreDeleteOptions extends
-      PipelineOptions,
-      CustomDatastoreReadOptions,
-      JavascriptTextTransformerOptions,
-      DatastoreDeleteOptions {}
-
-  /** Options for Reading Datastore Entities. */
-  public interface CustomDatastoreReadOptions extends PipelineOptions {
-    @Description("GQL Query which specifies what entities to grab")
-    String getDatastoreReadGqlQuery();
-    void setDatastoreReadGqlQuery(String datastoreReadGqlQuery);
-
-    @Description("Fake property to extract the NOW based GQL query")
-    @Default.String("DEFAULT")
-    default String getParsedGqlStatement() {
-      String gqlStatement = getDatastoreReadGqlQuery();
-      final Matcher matcher = PATTERN.matcher(gqlStatement);
-      while (matcher.find()) {
-        final String parsedDate = DatetimeHelper
-            .create()
-            .withNow(LocalDateTime.now())
-            .withOperation(matcher.group(1))
-            .withValue(matcher.group(2))
-            .withTimeUnit(matcher.group(3))
-            .build()
-            .calculate();
-        LOG.info("parsedDate: {}", parsedDate);
-
-        gqlStatement = gqlStatement.replaceFirst(NOW_PATTERN, parsedDate);
-        LOG.info("gqlStatement: {}", gqlStatement);
-      }
-      return gqlStatement;
-    }
-    void setParsedGqlStatement(String parsedGqlStatement);
-
-    @Description("GCP Project Id of where the datastore entities live")
-    ValueProvider<String> getDatastoreReadProjectId();
-    void setDatastoreReadProjectId(ValueProvider<String> datastoreReadProjectId);
-
-    @Description("Namespace of requested Entties. Set as \"\" for default namespace")
-    ValueProvider<String> getDatastoreReadNamespace();
-    void setDatastoreReadNamespace(ValueProvider<String> datstoreReadNamespace);
-  }
 
   /**
    * Runs a pipeline which reads in Entities from datastore, passes in the JSON encoded Entities to a Javascript UDF, and deletes all the Entities.
@@ -107,7 +59,11 @@ public class DatastoreToDatastoreDeleteCurrentDate {
 
     pipeline
         .apply(ReadJsonEntities.newBuilder()
-            .setGqlQuery(ValueProvider.StaticValueProvider.of(options.getParsedGqlStatement()))
+            .setGqlQuery(
+                NestedValueProvider.of(
+                    options.getDatastoreReadGqlQuery(),
+                    DatastoreToDatastoreDeleteCurrentDate::parseGqlStatement)
+            )
             .setProjectId(options.getDatastoreReadProjectId())
             .setNamespace(options.getDatastoreReadNamespace())
             .build())
@@ -120,6 +76,36 @@ public class DatastoreToDatastoreDeleteCurrentDate {
             .build());
 
     pipeline.run();
+  }
+
+  private static String parseGqlStatement(String gqlStatement) {
+    final Matcher matcher = PATTERN.matcher(gqlStatement);
+    while (matcher.find()) {
+      final String parsedDate = DatetimeHelper
+          .create()
+          .withNow(LocalDateTime.now())
+          .withOperation(matcher.group(1))
+          .withValue(matcher.group(2))
+          .withTimeUnit(matcher.group(3))
+          .build()
+          .calculate();
+      LOG.info("parsedDate: {}", parsedDate);
+
+      gqlStatement = gqlStatement.replaceFirst(PATTERN.pattern(), "'" + parsedDate + "'");
+      LOG.info("gqlStatement: {}", gqlStatement);
+    }
+    return gqlStatement;
+  }
+
+  /**
+   * Custom PipelineOptions.
+   */
+  public interface DatastoreToDatastoreDeleteOptions extends
+      PipelineOptions,
+      DatastoreReadOptions,
+      JavascriptTextTransformerOptions,
+      DatastoreDeleteOptions {
+
   }
 
 }
