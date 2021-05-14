9244e2006b783801d11f47d889e27f14afe52986
Xavier Capdevila <xavier.capdevila@fonyou.com>
2021-05-14 17:21:03 +0200

    Add more assertions and improve test

--- src/test/java/com/google/cloud/teleport/templates/custom/DatastoreToDatastoreDeleteCurrentDate.java
+++ src/test/java/com/google/cloud/teleport/templates/custom/DatastoreToDatastoreDeleteCurrentDate.java
@@ -1,9 +1,10 @@
 package com.google.cloud.teleport.templates.custom;
 
-import com.google.cloud.teleport.templates.custom.DatetimeHelper;
 import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,40 +16,49 @@ import org.junit.jupiter.api.Test;
 public class DatastoreToDatastoreDeleteCurrentDate {
 
   private static final String NOW_PATTERN = "\\$\\{NOW(\\+|\\-)(\\d*?)(y|m|d|h)\\}";
+  private static final String ALLOWED_OPERATIONS = "+-";
+  private static final String ALLOWED_TIMEUNITS = "ymdh";
 
   @Test
-  public void testRegex() {
-    String gqlStatement = "SELECT * FROM `Kind` WHERE timestamp < \"${NOW-5y}\" AND timestamp > \"${NOW-2m}\"";
-    System.out.println(gqlStatement.replaceAll(NOW_PATTERN, "Matched!"));
-    final String[] parts = Pattern.compile(NOW_PATTERN).split(gqlStatement);
-    for (String part : parts) {
-      System.out.println(part);
-    }
-    final Matcher matcher = Pattern.compile(NOW_PATTERN).matcher(gqlStatement);
+  public void testGqlNowRegexReplacement() {
+    String gqlStatement = "SELECT * FROM `Kind` WHERE timestamp < \"${NOW-5y}\" AND timestamp > \"${NOW+5y}\"";
+    final String replacement = "Matched!";
+    Assertions.assertTrue(gqlStatement.replaceAll(NOW_PATTERN, replacement).contains(replacement));
+
+    final Pattern pattern = Pattern.compile(NOW_PATTERN);
+    final Matcher matcher = pattern.matcher(gqlStatement);
     while (matcher.find()) {
-      System.out.println(matcher.groupCount());
-      for (int i = 0; i <= matcher.groupCount(); i++) {
-        System.out.println(i);
-        System.out.println(matcher.group(i));
-      }
+      final String operation = matcher.group(1);
+      final String value = matcher.group(2);
+      final String timeUnit = matcher.group(3);
+
+      Assertions.assertAll(
+          () -> Assertions.assertTrue(ALLOWED_OPERATIONS.contains(operation)),
+          () -> Assertions.assertTrue(StringUtils.isNumeric(value)),
+          () -> Assertions.assertTrue(ALLOWED_TIMEUNITS.contains(timeUnit.toLowerCase(Locale.ROOT)))
+      );
 
       final String parsedDate = DatetimeHelper
           .create()
           .withNow(LocalDateTime.now())
-          .withOperation(matcher.group(1))
-          .withValue(matcher.group(2))
-          .withTimeUnit(matcher.group(3))
+          .withOperation(operation)
+          .withValue(value)
+          .withTimeUnit(timeUnit)
           .build()
           .calculate();
-      System.out.println(
-          parsedDate);
+      if (operation.equals("+")) {
+        Assertions.assertEquals(LocalDateTime.now().plusYears(Long.parseLong(value)).toString().substring(0, 4), parsedDate.substring(0, 4));
+      } else {
+        Assertions.assertEquals(LocalDateTime.now().minusYears(Long.parseLong(value)).toString().substring(0, 4), parsedDate.substring(0, 4));
+      }
 
-      gqlStatement = gqlStatement.replaceFirst(NOW_PATTERN, parsedDate);
-      System.out.println(gqlStatement);
+      String previousGqlStatement = gqlStatement;
+      gqlStatement = gqlStatement.replaceFirst(pattern.pattern(), parsedDate);
+      Assertions.assertFalse(gqlStatement.equals(previousGqlStatement));
     }
 
-    System.out.println(gqlStatement);
-    Assertions.assertTrue(!gqlStatement.contains("${NOW"));
+    Assertions.assertFalse(gqlStatement.contains("${NOW"));
+    Assertions.assertFalse(gqlStatement.contains("}"));
   }
 
 }
